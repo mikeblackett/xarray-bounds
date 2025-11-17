3b290685097b2e6665493d971fe9732dd2e35ca9
mikeblackett <dev@mikeblackett.uk>
2025-11-17 13:35:18 +0000

    add version

--- src/xarray_bounds/__init__.py
+++ src/xarray_bounds/__init__.py
@@ -9,7 +9,7 @@ This module will undergo significant breaking changes when xarray adds support
 for ``CFIntervalIndex``. See https://github.com/pydata/xarray/pull/10296 for details.
 """
 
-from xarray_bounds import utilities
+from xarray_bounds._version import version as __version__
 from xarray_bounds.accessors import (
     DataArrayBoundsAccessor,
     DatasetBoundsAccessor,
