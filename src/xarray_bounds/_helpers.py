8e33411e2b959b0f936b2247370a94781b2de70f
mikeblackett <dev@mikeblackett.uk>
2025-12-15 16:36:38 +0000

    raise for invalid default closed values

--- src/xarray_bounds/_helpers.py
+++ src/xarray_bounds/_helpers.py
@@ -47,10 +47,7 @@ def validate_interval_closed(
     if closed is None and default is None:
         return ClosedSide.LEFT.value
     if closed is None:
-        try:
-            return ClosedSide(default).value
-        except ValueError:
-            return ClosedSide.LEFT.value
+        return ClosedSide(default).value
 
 
 def resolve_axis_name(obj: xr.Dataset | xr.DataArray, key: Hashable) -> str:
